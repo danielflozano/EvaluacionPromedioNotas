68aa87f69c9dfcd8afc232824c97ae72b1c83b41
Daniel Lozano <danyfriko@gmail.com>
2024-06-21 12:25:26 -0500

    Segundo Commit

--- src/Metodos.java
+++ src/Metodos.java
@@ -5,15 +5,18 @@ import javax.swing.JOptionPane;
 
 public class Metodos {
 	
-	int numEstudiantes = 0;
-	HashMap<String, Double> notasEstudiantes = new HashMap<String, Double>();
-		
-	public void iniciar() {
-		
+	HashMap<String, Double> notasEstudiantes; // Mapa para almacenar las notas de los estudiantes.
+	
+	// Constructor para inicializar el mapa de notas de estudiantes.
+	public Metodos() {
+		notasEstudiantes = new HashMap<String, Double>();
+	}
+	
+	// Método para inicializar y mostrar el menú.
+	public void inicializarMenu() {		
 		int menu = 0;
-		
 		do {
-			
+			// Mostrar el menú y obtener la opción seleccionada por el usuario.
 			menu = Integer.parseInt(JOptionPane.showInputDialog("****************************** MENU PRINCIPAL ******************************\n\n"
 					+ "1. Ingresar las notas de los estudiantes\n"
 					+ "2. Imprimir la Cantidad total de estudiantes validados\n"
@@ -25,225 +28,180 @@ public class Metodos {
 					+ "8. Imprimir el Promedio de las notas finales obtenidas\n"
 					+ "9. Imprimir el listado total de estudiantes y sus notas finales\n"
 					+ "10. Salir"));
-			
+			// Manejo de las opciones del menú.
 			switch (menu) {
 			case 1:
-				ingresarNotas();
+				solicitarNumEstudiantes(); // Solicitar y registrar las notas de los estudiantes.
 				break;
 			case 2:
-				imprimirTotalEstudiantes();
+				verificarHashMap(imprimirTotalEstudiantes()); // Imprimir la cantidad total de estudiantes.
 				break;
 			case 3:
-				imprimirTotalNotas();
+				verificarHashMap(imprimirTotalNotas()); // Imprimir la cantidad total de notas.
 				break;
 			case 4:
-				estudiantesGananMateria();
+				verificarHashMap(estudiantesGananMateria()); // Imprimir la cantidad total de estudiantes que ganan la materia.
 				break;
 			case 5:
-				estudiantesPierdenMateria();
+				verificarHashMap(estudiantesPierdenMateria()); // Imprimir la cantidad total de estudiantes que pierden la materia.
 				break;
 			case 6:
-				estudiantesPuedenRecuperarMateria();
+				verificarHashMap(estudiantesPuedenRecuperar()); // Imprimir la cantidad total de estudiantes que pueden recuperar la materia.
 				break;
 			case 7:
-				estudiantesNoPuedenRecuperarMateria();
+				verificarHashMap(estudiantesNoPuedenRecuperar()); // Imprimir la cantidad total de estudiantes que no pueden recuperar la materia.
 				break;
 			case 8:
-				promedioNotas();
+				verificarHashMap(promedioNotas()); // Imprimir el promedio de las notas finales.
 				break;
 			case 9:
-				imprimirLista();
+				listadoNotasEstudiantes(); // Imprimir el listado total de estudiantes y sus notas finales.);
 				break;
 			case 10:
-				JOptionPane.showMessageDialog(null, "Fin del programa");
+				JOptionPane.showMessageDialog(null, "Fin del programa"); // Salir del programa.
 				break;
 			default:
-				JOptionPane.showMessageDialog(null, "El valor que ha seleccionado no pertenece a ninguna opción, "
-						+ "Intente nuevamente");
+				JOptionPane.showMessageDialog(null, "El valor ingresado no corresponde a ninguna opción, "
+						+ "por favor digite un codigo valido", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE); // Manejo de opción inválida.
 				break;
-			}					
-			
-		} while (menu != 10);
-		
+			}			
+		} while (menu != 10);	
 	}
 	
-	private void ingresarNotas() {
-		
-		System.out.println("\n*****Ingreso De Notas*****");
-		
+	// Método para solicitar el número de estudiantes y sus notas.
+	private void solicitarNumEstudiantes() {
+		System.out.println("\n***** Ingreso Estudiantes Y Notas *****\n");
+		int numEstudiantes = 0;
 		do {
-			
-			numEstudiantes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de estudiantes a procesar"));
-			
+			numEstudiantes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de estudiantes que desea procesar"));
+			for (int i = 0; i < numEstudiantes; i++) {
+				alimentarHashMap(i); // Llenar el HashMap con las notas de los estudiantes.
+			}
+			if (numEstudiantes <= 0) {
+				JOptionPane.showMessageDialog(null, "Ingrese un número de estudiantes valido", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+			}			
 		} while (numEstudiantes <= 0);
-				
-		for (int i = 0; i < numEstudiantes; i++) {
-			
-			alimentarHashMap();		
-			
-		}
+		System.out.println("Datos Ingresados Correctamente");
 	}
 	
-	private void alimentarHashMap() {
-		
-		String nombreEstudiante = JOptionPane.showInputDialog("Ingrese el nombre del estudiante");
+	// Método para ingresar el nombre y las notas de un estudiante.
+	private HashMap<String, Double> alimentarHashMap(int numEstudiante) {
+		String nombreEstudiante = JOptionPane.showInputDialog("Ingrese el nombre del estudiante N° " + (numEstudiante + 1));
 		double sumatoriaNotas = 0;
-		double notaFinal = 0;
-		double nota = 0;			
-							
 		for (int i = 0; i < 3; i++) {
-			
-			nota = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota " + (i + 1)
+			double nota = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota N° " + (i+1)
 					+ " del estudiante " + nombreEstudiante));
-			
 			if (nota >= 0 && nota <= 5) {
-				
-				sumatoriaNotas += nota;				
-				
+				sumatoriaNotas += nota;
 			} else {
-				
-				System.out.println("La nota debe estar en un rango de 0 a 5");
-				
-				i--;
-				
+				JOptionPane.showMessageDialog(null, "La nota debe estar en un rango de 0 a 5", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+				i--; // Decrementar i para repetir la entrada de nota inválida.
+			}						
 			}
-			
+		double notaFinal = sumatoriaNotas/3;
+		notasEstudiantes.put(nombreEstudiante, notaFinal); // Almacenar la nota final del estudiante en el HashMap.
+		return notasEstudiantes;
+	}
+	
+	// Método para verificar si el HashMap esta vacio.
+	private void verificarHashMap(String metodo) {
+		if (notasEstudiantes.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "No se ha ingresado ningun dato", "ERROR", JOptionPane.ERROR_MESSAGE);
+		} else {
+			System.out.println(metodo);
 		}
-		
-		notaFinal = sumatoriaNotas / 3;
-		
-		notasEstudiantes.put(nombreEstudiante, notaFinal);
-		
 	}
 	
-	private void imprimirTotalEstudiantes() {
-		
-		System.out.println("\n*****Total Estudiantes*****\n");
-		
-		System.out.println("El total de estudiantes procesados es: " + notasEstudiantes.size());
-		
+	// Método para imprimir la cantidad total de estudiantes.
+	private String imprimirTotalEstudiantes() {
+		System.out.println("\n***** Cantidad Total De Estudiantes Validados *****\n");
+		int totalEstudiantes = notasEstudiantes.size();
+		return "La cantidad total de estudiantes procesados es: " + totalEstudiantes;
 	}
 	
-	private void imprimirTotalNotas() {
-		
-		System.out.println("\n*****Total Notas*****\n");
-		
+	// Método para imprimir la cantidad total de notas ingresadas.
+	private String imprimirTotalNotas() {
+		System.out.println("\n***** Cantidad Total De Notas Ingresadas *****\n");
 		int totalNotas = notasEstudiantes.size() * 3;
-		
-		System.out.println("El total de notas procesadas es : " + totalNotas);
-		
+		return "La cantidad total de notas ingresadas es: " + totalNotas;
 	}
 	
-	private void estudiantesGananMateria() {
-		
-		System.out.println("\n*****Total De Estudiantes Que Ganaron La Materia*****\n");
-		
-		int contadorEstudiantes = 0;
-		
+	// Método para imprimir la cantidad de estudiantes que ganan la materia.
+	private String estudiantesGananMateria() {
+		System.out.println("\n***** Cantidad De Estudiantes Que Ganan La Materia *****\n");
+		int sumaEstudiantes = 0;
 		Iterator<Double> iterador = notasEstudiantes.values().iterator();
-		
 		while (iterador.hasNext()) {
 			Double llave = iterador.next();
-			
 			if (llave >= 3.5) {
-				contadorEstudiantes += 1;
-			}
+				sumaEstudiantes ++;
+			}			
 		}
-		
-		System.out.println("La cantidad de estudiantes que ganaron la materia son: " + contadorEstudiantes);
-		
+		return "La cantidad de estudiantes que ganan la materia es: " + sumaEstudiantes;
 	}
 	
-	private void estudiantesPierdenMateria() {
-		
-		System.out.println("\n*****Total De Estudiantes Que Perdieron La Materia*****\n");
-		
-		int contadorEstudiantes = 0;
-		
+	// Método para imprimir la cantidad de estudiantes que pierden la materia.
+	private String estudiantesPierdenMateria() {
+		System.out.println("\n***** Cantidad De Estudiantes Que Pierden La Materia *****\n");
+		int sumaEstudiantes = 0;
 		Iterator<Double> iterador = notasEstudiantes.values().iterator();
-		
 		while (iterador.hasNext()) {
 			Double llave = iterador.next();
-			
 			if (llave < 3.5) {
-				contadorEstudiantes += 1;
-			}
+				sumaEstudiantes ++;
+			}			
 		}
-		
-		System.out.println("La cantidad de estudiantes que perdieron la materia son: " + contadorEstudiantes);
-		
+		return "La cantidad de estudiantes que pierden la materia es: " + sumaEstudiantes;
 	}
 	
-	private void estudiantesPuedenRecuperarMateria() {
-		
-		System.out.println("\n*****Total De Estudiantes Que Pueden Recuperar La Materia*****\n");
-		
-		int contadorEstudiantes = 0;
-		
+	// Método para imprimir la cantidad de estudiantes que pueden recuperar la materia.
+	private String estudiantesPuedenRecuperar() {
+		System.out.println("\n***** Cantidad De Estudiantes Que Pueden Recuperar La Materia *****\n");
+		int sumaEstudiantes = 0;
 		Iterator<Double> iterador = notasEstudiantes.values().iterator();
-		
 		while (iterador.hasNext()) {
 			Double llave = iterador.next();
-			
-			if (llave < 3.5 && llave >= 2) {
-				contadorEstudiantes += 1;
-			}
+			if (llave > 2 && llave < 3.5) {
+				sumaEstudiantes ++;
+			}			
 		}
-		
-		System.out.println("La cantidad de estudiantes que pueden recuperar la materia son: " + contadorEstudiantes);
-		
+		return "La cantidad de estudiantes que pueden recuperar la materia es: " + sumaEstudiantes;
 	}
 	
-	private void estudiantesNoPuedenRecuperarMateria() {
-		
-		System.out.println("\n*****Total De Estudiantes Que No Pueden Recuperar La Materia*****\n");
-		
-		int contadorEstudiantes = 0;
-		
+	// Método para imprimir la cantidad de estudiantes que no pueden recuperar la materia.
+	private String estudiantesNoPuedenRecuperar() {
+		System.out.println("\n***** Cantidad De Estudiantes Que No Pueden Recuperar La Materia *****\n");
+		int sumaEstudiantes = 0;
 		Iterator<Double> iterador = notasEstudiantes.values().iterator();
-		
 		while (iterador.hasNext()) {
 			Double llave = iterador.next();
-			
-			if (llave < 2) {
-				contadorEstudiantes += 1;
-			}
+			if (llave <= 2) {
+				sumaEstudiantes ++;
+			}			
 		}
-		
-		System.out.println("La cantidad de estudiantes que no pueden recuperar la materia son: " + contadorEstudiantes);
-		
+		return "La cantidad de estudiantes que no pueden recuperar la materia es: " + sumaEstudiantes;
 	}
 	
-	private void promedioNotas() {
-		
-		System.out.println("\n*****Promedio Notas*****\n");
-		
-		double notas = 0;
-		
+	// Método para imprimir el promedio de las notas finales.
+	private String promedioNotas() {
+		System.out.println("\n***** Promedio De Las Notas Finales *****\n");
+		double sumaNotas = 0;
 		Iterator<Double> iterador = notasEstudiantes.values().iterator();
-		
 		while (iterador.hasNext()) {
 			Double llave = iterador.next();
-			notas += llave;
+			sumaNotas += llave;
 		}
-		
-		double promedioNotas = notas / notasEstudiantes.size();
-		
-		System.out.println("El promedio de todas las notas es: " + promedioNotas);
-		
+		double promedioNotas = sumaNotas / notasEstudiantes.size();
+		return "El promedio de las notas finales es: " + promedioNotas;
 	}
 	
-	private void imprimirLista() {
-		
-		System.out.println("\n*****Lista Estudiantes*****\n");
-		
+	// Método para imprimir el listado de estudiantes y sus notas
+	private void listadoNotasEstudiantes() {
+		System.out.println("\n***** Listado De Estudiantes Y Sus Notas *****\n");
 		Iterator<String> iterador = notasEstudiantes.keySet().iterator();
-		
 		while (iterador.hasNext()) {
 			String llave = iterador.next();
 			System.out.println(llave + " - " + notasEstudiantes.get(llave));
 		}
-		
 	}
-		
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,10 +2,8 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		Metodos procesos = new Metodos();
-		procesos.iniciar();
-
+		System.out.println("Inicio Del Programa");
+		Metodos procesos = new Metodos(); // Crear una instancia de la clase Metodos.
+		procesos.inicializarMenu(); // Llamar al método que inicializa el menú.
 	}
-
 }
